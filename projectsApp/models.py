fc6d83e966dbd70afa1faaca7bed68cc385e7139
Adi Malca <adimalca91@gmail.com>
2024-02-07 10:27:04 +0200

    Added an str method to the Project Model

--- projectsApp/models.py
+++ projectsApp/models.py
@@ -12,4 +12,7 @@ class Project(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
     
     
+    def __str__(self):
+        return self.title
+    
     
