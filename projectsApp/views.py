2825a08c88d0e2a3f5543c32ba7769c82601fd8b
Adi Malca <adimalca91@gmail.com>
2024-02-07 16:44:44 +0200

    Added CRUD operations

--- projectsApp/views.py
+++ projectsApp/views.py
@@ -26,13 +26,42 @@ def project(request, pk):
     context = {'projectObject': projectObj}
     return render(request, 'projectsApp/single-project.html', context)
 
+'''
+CRUD - CREATE, READ, UPDATE, DELETE -OPERATIONS! 
+'''
+
+# Create and Read
 def createProject(request):
     form = ProjectForm()
     if request.method == "POST":
         form = ProjectForm(request.POST)
         if form.is_valid():
-            form.save()
+            form.save()       # CREATE A PROJECT OBJECT IN THE PROJECT MODEL
             return redirect('projects')
         
     context = {'form':form}
     return render(request, 'projectsApp/project_form.html', context)
+
+# Update and Read
+def updateProject(request, pk):
+    project = Project.objects.get(id=pk)
+    form = ProjectForm(instance=project)   # MUST pass in an instance of the project that we want to edit!
+    if request.method == "POST":
+        form = ProjectForm(request.POST, instance=project)
+        if form.is_valid():
+            form.save()       # CREATE A PROJECT OBJECT IN THE PROJECT MODEL
+            return redirect('projects')
+        
+    context = {'form':form}
+    return render(request, 'projectsApp/project_form.html', context)
+
+# Delete
+def deleteProject(request, pk):
+    project = Project.objects.get(id=pk)
+    if request.method == "POST":
+        project.delete()      # This will delete the project object from the DB / Model   
+        return redirect('projects') 
+    context = {'object' : project}
+    return render(request, 'projectsApp/delete_template.html', context)
+    
+    
