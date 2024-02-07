0c779c5a8a81144fec7da38a669f41f349568c9a
Adi Malca <adimalca91@gmail.com>
2024-02-07 10:25:46 +0200

    Added a model and registered it

--- projectsApp/admin.py
+++ projectsApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from.models import Project
 
 # Register your models here.
+
+# Telling Django - get this model and show it in the admin panel interface
+admin.site.register(Project) 
+
--- projectsApp/models.py
+++ projectsApp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=200)   # By default null=False - required to enter data in this field (for the database)
+    description = models.TextField(null=True, blank=True)  # Does NOT require to enter data in this field! blank is the same but for django form to be submitted! We can submit a form without adding this!
+    demo_link = models.CharField(max_length=2000, null=True, blank=True)
+    source_link = models.CharField(max_length=2000, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    
+    
+    
