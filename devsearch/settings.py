d53a73ef2626473c59dc2b5def68f6e8c532b0d7
Adi Malca <adimalca91@gmail.com>
2024-02-09 19:12:26 +0200

    Added static folder and files, added link to template

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -120,6 +120,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Adi: Added this in order for Django to know where the static folder is
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
