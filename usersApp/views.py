32a84d26bd5851008bca956b6ea84cdf498b9962
Adi Malca <adimalca91@gmail.com>
2024-02-13 11:53:59 +0200

    Added user profile theme and adjusted it

--- usersApp/views.py
+++ usersApp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .models import Profile
 
 # Create your views here.
 
 def profiles(request):
-    return render(request, 'usersApp/profiles.html')
+    profiles = Profile.objects.all()  # Retrieve all the profiles in the db / model - QuerySet object
+    context = {'profiles':profiles}
+    return render(request, 'usersApp/profiles.html', context)
