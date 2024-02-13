fa2b5102a1748373917ca230aeebb659cf737073
Adi Malca <adimalca91@gmail.com>
2024-02-13 22:09:46 +0200

    created and connected user and profile create and delete signals

--- usersApp/models.py
+++ usersApp/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User
 import uuid
 
+from django.db.models.signals import post_save  # This method is going to be triggered / invoked any time a model is saved already and after it's saved!
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
+
 # Create your models here.
 
 '''
@@ -28,7 +32,7 @@ class Profile(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
     
     def __str__(self):
-        return str(self.user.username)
+        return str(self.username)
     
     
 
@@ -43,3 +47,44 @@ class Skill(models.Model):
         return self.name
     
     
+# Django Signals
+'''
+Create some reciever and sender that will fire off anytime the save() method is called on a user profile
+'''
+
+'''
+Arguments:
+sender - The model that actually sends this
+instance - The instance of the model that actually triggered this (the object)
+created - Boolean that lets us know if a user / a model was added to the db or if it was simply saved again
+        - lets us know if a new record in the db was added or not.
+'''
+#@receiver(post_save, sender=Profile)
+def createProfile(sender, instance, created, **kwargs):
+    if created:
+        user = instance
+        profile = Profile.objects.create(
+            user=user,
+            username=user.username,
+            email=user.email,
+            name=user.first_name,
+            )
+
+'''
+Anytime we delete a profile we also want to delete a user
+Note - when delete is called on a user instance then it automatically deletes the user's profile b/c the 
+one-to-one CASCASDE relationship!
+But, if the profile gets deleted then now with this method, the user associated with that profile 
+also gets deleted!
+'''
+@receiver(post_delete, sender=Profile)
+def deleteUser(sender, instance, **kwargs):
+    user = instance.user    # the instance here is a Profile instance - so instance.user is the user associated with that profile!
+    user.delete()
+
+# Any time a user model instance gets created a profile will be created
+# Any time a user gets created what do we want to do?
+post_save.connect(createProfile, sender=User)
+
+
+post_delete.connect(deleteUser, sender=Profile)
