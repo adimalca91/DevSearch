aca42e4379334a21fbde0e5532e1442acad0a371
Adi Malca <adimalca91@gmail.com>
2024-02-13 12:49:33 +0200

    Changed source for Icons in main css file

--- usersApp/views.py
+++ usersApp/views.py
@@ -3,10 +3,19 @@ from .models import Profile
 
 # Create your views here.
 
+'''
+Get all users
+'''
 def profiles(request):
     profiles = Profile.objects.all()  # Retrieve all the profiles in the db / model - QuerySet object
     context = {'profiles':profiles}
     return render(request, 'usersApp/profiles.html', context)
 
+
+'''
+Get the specific user
+'''
 def userProfile(request, pk):
-    return render(request, 'usersApp/user-profile.html')
+    profile = Profile.objects.get(id=pk)
+    context = {'profile':profile}
+    return render(request, 'usersApp/user-profile.html', context)
