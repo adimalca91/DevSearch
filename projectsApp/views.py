74bc346e7c7bfdc193781c487b6707618f41daed
Adi Malca <adimalca91@gmail.com>
2024-02-19 17:42:43 +0200

    Added more search functionality to projects view - search also by tags

--- projectsApp/views.py
+++ projectsApp/views.py
@@ -18,16 +18,20 @@ def projects(request):
     # Check if something was searched in the search bar
     if request.GET.get('search_query'):
         search_query = request.GET.get('search_query')
+        
+    tags = Tag.objects.filter(name__icontains=search_query)  # The project model has a tags field which is many-to-many field
     
     
     # projects = Project.objects.all()  # QuerySet - dictionary like object
     
-    projects = Project.objects.filter(
+    # distinct() to prevent duplicates!
+    projects = Project.objects.distinct().filter(
         Q(title__icontains=search_query) |
         Q(description__icontains=search_query) |
-        Q(owner__name__icontains=search_query)  # here we go one level up and we query by the parent item. 
+        Q(owner__name__icontains=search_query) | # here we go one level up and we query by the parent item. 
         # we go into the parent object (owner) and then into the attribute of 'name'
         # give us every project where the owner's name contains 'search_query'
+        Q(tags__in=tags)
     )
     
     context = {'projects': projects, 'search_query':search_query}
