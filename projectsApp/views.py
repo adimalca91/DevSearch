a7c7e2c64d8ca37a0aa424a1d737f9d64e8741f8
Adi Malca <adimalca91@gmail.com>
2024-02-19 17:49:34 +0200

    Added a helper functions file called utils

--- projectsApp/views.py
+++ projectsApp/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.decorators import login_required     # This decorator w
 from .models import *
 from .forms import *
 from django.db.models import Q
+from .utils import searchProjects
 
 # Create your views here.
 
@@ -13,26 +14,7 @@ Pass in some dynamic data to the template to render and display in the browser
 '''
 def projects(request):
     
-    search_query = ""
-    
-    # Check if something was searched in the search bar
-    if request.GET.get('search_query'):
-        search_query = request.GET.get('search_query')
-        
-    tags = Tag.objects.filter(name__icontains=search_query)  # The project model has a tags field which is many-to-many field
-    
-    
-    # projects = Project.objects.all()  # QuerySet - dictionary like object
-    
-    # distinct() to prevent duplicates!
-    projects = Project.objects.distinct().filter(
-        Q(title__icontains=search_query) |
-        Q(description__icontains=search_query) |
-        Q(owner__name__icontains=search_query) | # here we go one level up and we query by the parent item. 
-        # we go into the parent object (owner) and then into the attribute of 'name'
-        # give us every project where the owner's name contains 'search_query'
-        Q(tags__in=tags)
-    )
+    projects, search_query = searchProjects(request)
     
     context = {'projects': projects, 'search_query':search_query}
     return render(request, 'projectsApp/projects.html', context)
