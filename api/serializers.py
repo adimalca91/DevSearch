d3b6252a9f36859ffbfa76c1eb9fa8f580405126
Adi Malca <adimalca91@gmail.com>
2024-02-26 17:51:51 +0200

    Added api urls, views and serializers

--- api/serializers.py
+++ api/serializers.py
@@ -1,2 +1,18 @@
 # We need to serialize data before we return it
 # Take python models and serialize them
+
+# Serialization is the process of converting our data - those python objects will be converted to Json objects
+
+# Here we will work with Model Serialization
+
+from rest_framework import serializers
+from projectsApp.models import Project
+
+
+'''
+This is a Model Serializer - it will take the Project Model and will serialize it (convert it) into a Json object
+'''
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = '__all__'
--- api/views.py
+++ api/views.py
@@ -1,8 +1,14 @@
 from django.http import JsonResponse     # Takes any python data that we have, and will turn it into json data
+from rest_framework.decorators import api_view
+from rest_framework.response import Response 
+from .serializers import ProjectSerializer
+from projectsApp.models import Project
 
 
 # A view that will tell us all the url paths / toutes that we are going to have inside of our api's
 # Kind of like documentation for api
+
+@api_view(['GET'])  # specify this as a GET method
 def getRoutes(request):
     
     # A list of python dicts that will later turn to JS objects
@@ -16,5 +22,19 @@ def getRoutes(request):
         {'POST':'/api/users/token/refresh'},
     ]
     
-    return JsonResponse(routes, safe=False)    # JsonResponse will give us Json data; safe - tells we can return back something that is more than just a py dict
+    return Response(routes)                     # JsonResponse will give us Json data; safe - tells we can return back something that is more than just a py dict
                                                # By default the JsonResponse can only return back a py dict, but b/c we are sending a LIST of dicts we need to set safe to False - go ahead and turn any kind of data that we want into Json data
+
+                                              
+@api_view(['GET'])    
+def getProjects(request):
+    projects = Project.objects.all()                     # Query Project model to retrieve all the project objects
+    serializer = ProjectSerializer(projects, many=True)  # Takes the queryset and turn it into json data; many - for serializing many objects
+    return Response(serializer.data)
+
+
+@api_view(['GET'])    
+def getProject(request, pk):
+    project = Project.objects.get(id=pk)                 # Query Project model to retrieve a single project object
+    serializer = ProjectSerializer(project, many=False)  # Takes the queryset and turn it into json data; many - for serializing many objects
+    return Response(serializer.data)
