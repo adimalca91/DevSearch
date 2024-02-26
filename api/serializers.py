70f7890ccb23105833b8af22d59dddeb730c8503
Adi Malca <adimalca91@gmail.com>
2024-02-26 18:10:11 +0200

    Created relationship between serializers

--- api/serializers.py
+++ api/serializers.py
@@ -6,13 +6,39 @@
 # Here we will work with Model Serialization
 
 from rest_framework import serializers
-from projectsApp.models import Project
+from projectsApp.models import Project, Tag, Review
+from usersApp.models import Profile
 
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = '__all__'
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = '__all__'
+        
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = '__all__'
 
 '''
 This is a Model Serializer - it will take the Project Model and will serialize it (convert it) into a Json object
 '''
 class ProjectSerializer(serializers.ModelSerializer):
+    owner = ProfileSerializer(many=False)
+    tags = TagSerializer(many=True)
+    reviews = serializers.SerializerMethodField()     # the method then has to start with 'get_'
+    
     class Meta:
         model = Project
         fields = '__all__'
+        
+    def get_reviews(self, obj):
+        reviews = obj.review_set.all()
+        serializer = ReviewSerializer(reviews, many=True)
+        return serializer.data
